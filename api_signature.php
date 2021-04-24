5c2fe950d5e8d49531985a1aeef7f49f5dd7af6c
HRie <hariajie@gmail.com>
2021-04-24 23:45:18 +0700

    Update Poll

--- api_signature.php
+++ api_signature.php
@@ -1,4 +1,61 @@
 <?php
+function urutinProv($array, $strict = null)
+{
+    $orderArray = array(
+        "Aceh",
+        "Sumatera Utara",
+        "Sumatera Barat",
+        "Riau",
+        "Jambi",
+        "Sumatera Selatan",
+        "Bengkulu",
+        "Lampung",
+        "Kepulauan Bangka Belitung",
+        "Kepulauan Riau",
+        "Dki Jakarta",
+        "Jawa Barat",
+        "Jawa Tengah",
+        "Daerah Istimewa Yogyakarta",
+        "Jawa Timur",
+        "Banten",
+        "Bali",
+        "Nusa Tenggara Barat",
+        "Nusa Tenggara Timur",
+        "Kalimantan Barat",
+        "Kalimantan Tengah",
+        "Kalimantan Selatan",
+        "Kalimantan Timur",
+        "Kalimantan Utara",
+        "Sulawesi Utara",
+        "Sulawesi Tengah",
+        "Sulawesi Selatan",
+        "Sulawesi Tenggara",
+        "Gorontalo",
+        "Sulawesi Barat",
+        "Maluku",
+        "Maluku Utara",
+        "Papua",
+        "Papua Barat"
+    );
+
+    if (!empty($array) && !empty($orderArray)) {
+        $ordered = [];
+        foreach ($orderArray as $item) {
+            $search = !is_null($strict) ? array_keys($array, $item, $strict) : array_keys(preg_grep('#' . $item . '#', $array));
+            if (!empty($search)) {
+                foreach ($search as $key) {
+                    $ordered[$key] = $array[$key];
+                    unset($array[$key]);
+                }
+            }
+        }
+
+        return (object) array_merge($ordered, $array);
+    } else {
+        return array();
+    }
+}
+
 function get_cuaca($param)
 {
     $url = "https://signature.bmkg.go.id/api/signature/impact/public/list/" . $param;
@@ -18,6 +75,12 @@ function get_cuaca($param)
         "type" => "FeatureCollection",
         "features" => array()
     );
+
+    //Array ke view
+    $resp = array("data" => array("awas" => array(), "siaga" => array(), "waspada" => array()), "geojson" => array());
+    $tmp_awas = array();
+    $tmp_siaga = array();
+    $tmp_waspada = array();
 
     //Convert to geoJSON
 
@@ -43,11 +106,44 @@ function get_cuaca($param)
                 "status" => $status
             )
         );
-
         array_push($geojson["features"], $push);
+
+        //Insert Data
+
+        if ($status == "Awas") {
+            foreach (array_keys($row["impacted"]) as $val) {
+                $tmp_awas[] = $val;
+            }
+        } else if ($status == "Siaga") {
+            foreach (array_keys($row["impacted"]) as $val) {
+                $tmp_siaga[] = $val;
+            }
+        } else if ($status == "Waspada") {
+            foreach (array_keys($row["impacted"]) as $val) {
+                $tmp_waspada[] = $val;
+            }
+        }
     }
 
-    return json_encode($geojson, JSON_PRETTY_PRINT);
+    //Biar ga redundan
+    foreach ($tmp_awas as $awas) {
+        array_push($resp["data"]["awas"], $awas);
+    }
+    foreach ($tmp_siaga as $siaga) {
+        array_push($resp["data"]["siaga"], $siaga);
+    }
+    foreach ($tmp_waspada as $wasp) {
+        array_push($resp["data"]["waspada"], $wasp);
+    }
+
+    $resp["data"]["awas"] = urutinProv(array_unique($resp["data"]["awas"]), true);
+    $resp["data"]["siaga"] = urutinProv(array_unique($resp["data"]["siaga"]), true);
+    $resp["data"]["waspada"] = urutinProv(array_unique($resp["data"]["waspada"]), true);
+
+
+    $resp["geojson"] = $geojson;
+
+    return json_encode($resp, JSON_PRETTY_PRINT);
 }
 
 echo get_cuaca($_GET["tgl"]);
---
+++ infoprediksi.js
@@ -0,0 +1,150 @@
+let table = {};
+
+function init_map() {
+  let map = L.map("mapid").setView([-1.2828926, 119.1433416], 5);
+
+  //Siapin variable besok
+  let date = new Date();
+  // Add a day
+  let besok = date.setDate(date.getDate());
+  let tgl_besok = date.toISOString().split("T")[0];
+  besok = tgl_besok + "T00:00:00.000Z";
+  let geojsonLayer;
+
+  L.tileLayer.provider("GoogleHybrid").addTo(map);
+
+  $.ajax({
+    url: "http://localhost/api-signature/api_signature.php?tgl=" + besok,
+    method: "GET",
+    dataType: "json",
+    beforeSend: function () {
+      $("#loading").show();
+    },
+    success: function (response) {
+      console.log(response);
+      let geojson = response.geojson;
+      let geoJsonLayer = new L.GeoJSON(geojson, {
+        style: function (feature) {
+          let cat = feature.properties.category;
+          let warna, style;
+          cat = parseInt(cat);
+          //console.log("Cate :" + cat)
+
+          if (cat == 10) {
+            warna = "#db0000";
+          } else if (cat > 5) {
+            warna = "#f09835";
+          } else if (cat > 0) {
+            warna = "#fff157";
+          }
+
+          // console.log(warna);
+
+          style = {
+            color: warna,
+            weight: 0,
+            opacity: 0.7,
+            fillOpacity: 0.7,
+          };
+
+          return style;
+        },
+        onEachFeature: function (feature, layer) {
+          //Bikin list kab
+          let impact = feature.properties.impacted;
+          console.log("Impact");
+          console.log(Object.keys(impact));
+          let tmp = {};
+          for (const [key, val] of Object.entries(impact)) {
+            tmp[key] = Object.keys(val);
+            // for (const [k, v] of Object.entries(impact[key])) {
+            //   tmp[key].push(k);
+            // }
+          }
+          let list = {
+            prov: tmp,
+            status: feature.properties.status,
+          };
+          console.log(tmp);
+
+          let sta;
+          if (list.status == "Awas") {
+            sta = "<span class='badge awas text-dark'>Awas</span>";
+          } else if (list.status == "Siaga") {
+            sta = "<span class='badge siaga text-dark'>Siaga</span>";
+          } else if (list.status == "Waspada") {
+            sta = "<span class='badge waspada text-dark'>Waspada</span>";
+          }
+
+          let content = "<div class='fs-6'>Status : " + sta + "<br><br>";
+          content +=
+            "<table class='table table-sm table-responsive table-bordered'>";
+          content += "<thead class='bg-info fw-bold'>";
+          content += "<tr><td>Provinsi</td><td>Kab/Kota</td></tr></thead>";
+          content += "<tbody>";
+          for (const [key, val] of Object.entries(list.prov)) {
+            content += "<tr><td>" + key + "</td><td>";
+            for (const [k, v] of Object.entries(list.prov[key])) {
+              content += v + "<br>";
+            }
+            content += "</td></tr>";
+          }
+          content += "</tbody>";
+          content += "</table>";
+          content += "</div>";
+          let popup = L.popup().setContent(content);
+          layer.bindPopup(popup, { minWidth: 350 });
+        },
+      }).addTo(map);
+      $("#loading").hide();
+      table = response.data;
+      list_table(table);
+    },
+  });
+  $(".tanggal").html("tanggal " + "<b>" + format_tanggal(tgl_besok) + "</b>");
+}
+
+function list_table(table) {
+  let html = "<table class='table table-sm table-responsive table-bordered'>";
+  html += "<thead class='bg-primary text-white fw-bold'>";
+  html += "<tr><td>Status</td><td>Provinsi</td></tr></thead>";
+  html += "<tbody>";
+  if (Object.keys(table.awas).length > 0) {
+    html += "<tr class='awas'><td>Awas</td>";
+    html += "<td>";
+    for (const [key, val] of Object.entries(table.awas)) {
+      html += val + "<br>";
+    }
+    html += "</td></tr>";
+  }
+  if (Object.keys(table.siaga).length > 0) {
+    html += "<tr class='siaga'><td>Siaga</td>";
+    html += "<td>";
+    for (const [key, val] of Object.entries(table.siaga)) {
+      html += val + "<br>";
+    }
+    html += "</td></tr>";
+  }
+  if (Object.keys(table.waspada).length > 0) {
+    html += "<tr class='waspada'><td>Waspada</td>";
+    html += "<td >";
+    for (const [key, val] of Object.entries(table.waspada)) {
+      html += val + "<br>";
+    }
+    html += "</td></tr>";
+  }
+  if (
+    Object.keys(table.waspada).length == 0 &&
+    Object.keys(table.awas).length == 0 &&
+    Object.keys(table.siaga).length == 0
+  ) {
+    html += "<tr class='text-center'><td colspan='2'>Tidak ada data.</td>";
+    html += "</td></tr>";
+  }
+  html += "</tbody>";
+  html += "</table>";
+
+  $(".prov-list").html(html);
+}
+
+init_map();
